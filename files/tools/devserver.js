0dc739272365ebd80014e3af582c8ab1bc5d2db1
Koto Furumiya <kotofurumiya@gmail.com>
2020-11-21 17:35:31 +0900

    devserver now uses esbuild incremental build

--- files/tools/devserver.js
+++ files/tools/devserver.js
@@ -2,11 +2,14 @@ const esbuild = require('esbuild');
 const cpx = require('cpx');
 const bs = require('browser-sync').create();
 
+let initialBuild;
+
 const build = async () => {
   return esbuild.build({
     entryPoints: ['./src/index.tsx'],
     outfile: `./build/index.js`,
     minify: process.env.NODE_ENV === 'production',
+    incremental: true,
     bundle: true,
     sourcemap: process.env.NODE_ENV === 'development',
     define: {
@@ -16,7 +19,9 @@ const build = async () => {
 };
 
 bs.watch('src/**/*.{js,jsx,ts,tsx}', (event, file) => {
-  build()
+  const currentBuild = initialBuild?.rebuild() || build();
+  
+  currentBuild
     .then(() => bs.reload('*.js'))
     .catch((e) => console.error(e));
 });
